28cbcd632edeaad23d583ced796625dd95d4cc35
dunhamik000 <127815931+dunhamik000@users.noreply.github.com>
2023-04-12 03:32:02 +0000

    CodeWordChecker and StepTracker homework

--- unit11/CodeWordChecker.java
+++ unit11/CodeWordChecker.java
@@ -9,12 +9,28 @@ interface StringChecker {
 
 public class CodeWordChecker implements StringChecker {
 
-    // TODO implement me!
+    private int min;
+    private int max;
+    private String no;
+
+    public CodeWordChecker(int a, int b, String c){
+        min=a;
+        max=b;
+        no=c;
+    }
+
+    public CodeWordChecker(String x){
+        min=6;
+        max=20;
+        no=x;
+    }
 
     /** Returns true if str is valid. */
     public boolean isValid(String str) {
-        // TODO
-        return false; // replace me!
+        if((str.indexOf(no)!=-1)||(str.length()<min)||(str.length()>max)){
+            return false;
+        }
+        return true; 
     }
 
     static boolean check(boolean test) throws AssertionError {
@@ -25,21 +41,21 @@ public class CodeWordChecker implements StringChecker {
 
     public static void main(String[] args) {
         // uncomment the following lines to test your code
-        // StringChecker sc1 = new CodeWordChecker(5, 8, "$");
-        // check(sc1.isValid("happy"));
-        // check(!sc1.isValid("happy$"));
-        // check(!sc1.isValid("Code"));
-        // check(!sc1.isValid("happyCode"));
-        // check(!sc1.isValid("happy$Code"));
-        // check(!sc1.isValid("Code$happy"));
+        StringChecker sc1 = new CodeWordChecker(5, 8, "$");
+        check(sc1.isValid("happy"));
+        check(!sc1.isValid("happy$"));
+        check(!sc1.isValid("Code"));
+        check(!sc1.isValid("happyCode"));
+        check(!sc1.isValid("happy$Code"));
+        check(!sc1.isValid("Code$happy"));
 
-        // StringChecker sc2 = new CodeWordChecker("pass");
-        // check(sc2.isValid("MyPass"));
-        // check(!sc2.isValid("Mypassport"));
-        // check(!sc2.isValid("happy"));
-        // check(!sc2.isValid("1,000,000,000,000,000"));
+        StringChecker sc2 = new CodeWordChecker("pass");
+        check(sc2.isValid("MyPass"));
+        check(!sc2.isValid("Mypassport"));
+        check(!sc2.isValid("happy"));
+        check(!sc2.isValid("1,000,000,000,000,000"));
 
-        // System.out.println("Happy Panda! \uD83D\uDC3C");
+        System.out.println("Happy Panda! \uD83D\uDC3C");
     }
 
 }
--- unit11/StepTracker.java
+++ unit11/StepTracker.java
@@ -4,7 +4,36 @@ package unit11;
 // https://apcentral.collegeboard.org/media/pdf/ap19-frq-computer-science-a.pdf#page=7
 public class StepTracker {
 
-    // TODO implement me!
+    private int steps;
+    private int days;
+    private int activeDay;
+    private int min;
+
+    public StepTracker(int x){
+        min=x;
+        days=0;
+        activeDay=0;
+        steps=0;
+    }
+
+    public void addDailySteps(int a){
+        days++;
+        steps=steps+a;
+        if(a>=min){
+            activeDay++;
+        }
+    }
+
+    public int activeDays(){
+        return activeDay;
+    }
+
+    public double averageSteps(){
+        if(days==0){
+            return 0.0;
+        }
+        return 1.0*steps/days;
+    }
 
     public static void check(boolean test) throws AssertionError {
         if (!test)
@@ -13,20 +42,20 @@ public class StepTracker {
 
     public static void main(String[] args) {
         // uncomment the following lines to test your code
-        // StepTracker tr = new StepTracker(10000);
-        // check(tr.activeDays() == 0);
-        // check(tr.averageSteps() == 0.0);
-        // tr.addDailySteps(9000);
-        // tr.addDailySteps(5000);
-        // check(tr.activeDays() == 0);
-        // check(tr.averageSteps() == 7000.0);
-        // tr.addDailySteps(13000);
-        // check(tr.activeDays() == 1);
-        // check(tr.averageSteps() == 9000.0);
-        // tr.addDailySteps(23000);
-        // tr.addDailySteps(1111);
-        // check(tr.activeDays() == 2);
-        // check(tr.averageSteps() == 10222.2);
-        // System.out.println("Happy Panda! \uD83D\uDC3C");
+        StepTracker tr = new StepTracker(10000);
+        check(tr.activeDays() == 0);
+        check(tr.averageSteps() == 0.0);
+        tr.addDailySteps(9000);
+        tr.addDailySteps(5000);
+        check(tr.activeDays() == 0);
+        check(tr.averageSteps() == 7000.0);
+        tr.addDailySteps(13000);
+        check(tr.activeDays() == 1);
+        check(tr.averageSteps() == 9000.0);
+        tr.addDailySteps(23000);
+        tr.addDailySteps(1111);
+        check(tr.activeDays() == 2);
+        check(tr.averageSteps() == 10222.2);
+        System.out.println("Happy Panda! \uD83D\uDC3C");
     }
 }
